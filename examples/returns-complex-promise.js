d61d4e000c3d23a5dac21464bdebbd69962bbebe
Jun Matsushita <jun@iilab.org>
2017-05-16 21:28:48 +0200

    Add ~> for specifying promise and get aroung mocha overspecified reoslution problem. Adds a chai-as-promised implicit dep.

---
+++ examples/returns-complex-promise.js
@@ -0,0 +1,14 @@
+var Promise = require('bluebird');
+
+/**
+ * @example
+ *   resolvesAPromise()
+ *    .then(() => resolvesAPromise())
+ *    // ~> 10
+ */
+
+function resolvesAPromise() {
+  return new Promise(function(resolve) {
+    resolve(10);
+  });
+}
